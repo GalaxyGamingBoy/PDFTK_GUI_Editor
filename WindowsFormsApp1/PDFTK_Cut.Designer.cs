c75d4280c06067a30322bfea2cb384a3df97b377
GalaxyGamingBoy <xrteach@gmail.com>
2022-06-06 13:24:42 +0300

    Init Commit
    Added PDFTK_Cut

---
+++ WindowsFormsApp1/PDFTK_Cut.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class PDFTK_CUT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFTK_CUT));
+            this.inputPDF = new System.Windows.Forms.Label();
+            this.OpenPDFFile = new System.Windows.Forms.Button();
+            this.openPDFDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileLocation = new System.Windows.Forms.Label();
+            this.savePDFDialog = new System.Windows.Forms.SaveFileDialog();
+            this.outputPDF = new System.Windows.Forms.Label();
+            this.outFileLocation = new System.Windows.Forms.Label();
+            this.SavePDFFile = new System.Windows.Forms.Button();
+            this.pageSelector = new System.Windows.Forms.Label();
+            this.pageTXTBox = new System.Windows.Forms.TextBox();
+            this.pagesValid = new System.Windows.Forms.PictureBox();
+            this.RunSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pagesValid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputPDF
+            // 
+            this.inputPDF.AutoSize = true;
+            this.inputPDF.Font = new System.Drawing.Font("Roboto", 13F, System.Drawing.FontStyle.Bold);
+            this.inputPDF.Location = new System.Drawing.Point(13, 13);
+            this.inputPDF.Name = "inputPDF";
+            this.inputPDF.Size = new System.Drawing.Size(132, 22);
+            this.inputPDF.TabIndex = 0;
+            this.inputPDF.Text = "Input PDF File: ";
+            // 
+            // OpenPDFFile
+            // 
+            this.OpenPDFFile.Font = new System.Drawing.Font("Roboto", 8F);
+            this.OpenPDFFile.Location = new System.Drawing.Point(617, 12);
+            this.OpenPDFFile.Name = "OpenPDFFile";
+            this.OpenPDFFile.Size = new System.Drawing.Size(75, 23);
+            this.OpenPDFFile.TabIndex = 1;
+            this.OpenPDFFile.Text = "Open";
+            this.OpenPDFFile.UseVisualStyleBackColor = true;
+            this.OpenPDFFile.Click += new System.EventHandler(this.OpenPDFFile_Click);
+            // 
+            // openPDFDialog
+            // 
+            this.openPDFDialog.FileName = "inputFile";
+            this.openPDFDialog.Filter = "PDF File|*.pdf";
+            this.openPDFDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openPDFDialog_FileOk);
+            // 
+            // fileLocation
+            // 
+            this.fileLocation.AutoSize = true;
+            this.fileLocation.Font = new System.Drawing.Font("Roboto", 13F, System.Drawing.FontStyle.Italic);
+            this.fileLocation.Location = new System.Drawing.Point(151, 13);
+            this.fileLocation.Name = "fileLocation";
+            this.fileLocation.Size = new System.Drawing.Size(120, 22);
+            this.fileLocation.TabIndex = 2;
+            this.fileLocation.Text = ".\\inputFile.pdf";
+            // 
+            // savePDFDialog
+            // 
+            this.savePDFDialog.FileName = "outputFile.pdf";
+            this.savePDFDialog.Filter = "PDF File|*.pdf";
+            this.savePDFDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.savePDFDialog_FileOk);
+            // 
+            // outputPDF
+            // 
+            this.outputPDF.AutoSize = true;
+            this.outputPDF.Font = new System.Drawing.Font("Roboto", 13F, System.Drawing.FontStyle.Bold);
+            this.outputPDF.Location = new System.Drawing.Point(12, 53);
+            this.outputPDF.Name = "outputPDF";
+            this.outputPDF.Size = new System.Drawing.Size(145, 22);
+            this.outputPDF.TabIndex = 3;
+            this.outputPDF.Text = "Output PDF File: ";
+            // 
+            // outFileLocation
+            // 
+            this.outFileLocation.AutoSize = true;
+            this.outFileLocation.Font = new System.Drawing.Font("Roboto", 13F, System.Drawing.FontStyle.Italic);
+            this.outFileLocation.Location = new System.Drawing.Point(163, 53);
+            this.outFileLocation.Name = "outFileLocation";
+            this.outFileLocation.Size = new System.Drawing.Size(132, 22);
+            this.outFileLocation.TabIndex = 4;
+            this.outFileLocation.Text = ".\\outputFile.pdf";
+            // 
+            // SavePDFFile
+            // 
+            this.SavePDFFile.Font = new System.Drawing.Font("Roboto", 8F);
+            this.SavePDFFile.Location = new System.Drawing.Point(617, 54);
+            this.SavePDFFile.Name = "SavePDFFile";
+            this.SavePDFFile.Size = new System.Drawing.Size(75, 23);
+            this.SavePDFFile.TabIndex = 5;
+            this.SavePDFFile.Text = "Open";
+            this.SavePDFFile.UseVisualStyleBackColor = true;
+            this.SavePDFFile.Click += new System.EventHandler(this.SavePDFFile_Click);
+            // 
+            // pageSelector
+            // 
+            this.pageSelector.AutoSize = true;
+            this.pageSelector.Font = new System.Drawing.Font("Roboto", 13F, System.Drawing.FontStyle.Bold);
+            this.pageSelector.Location = new System.Drawing.Point(13, 144);
+            this.pageSelector.Name = "pageSelector";
+            this.pageSelector.Size = new System.Drawing.Size(125, 22);
+            this.pageSelector.TabIndex = 6;
+            this.pageSelector.Text = "Select Pages: ";
+            // 
+            // pageTXTBox
+            // 
+            this.pageTXTBox.Font = new System.Drawing.Font("Roboto", 12F);
+            this.pageTXTBox.Location = new System.Drawing.Point(144, 142);
+            this.pageTXTBox.Name = "pageTXTBox";
+            this.pageTXTBox.Size = new System.Drawing.Size(116, 27);
+            this.pageTXTBox.TabIndex = 7;
+            this.pageTXTBox.Text = "1-1";
+            this.pageTXTBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.pageTXTBox.TextChanged += new System.EventHandler(this.pageTXTBox_TextChanged);
+            // 
+            // pagesValid
+            // 
+            this.pagesValid.Image = ((System.Drawing.Image)(resources.GetObject("pagesValid.Image")));
+            this.pagesValid.Location = new System.Drawing.Point(266, 137);
+            this.pagesValid.Name = "pagesValid";
+            this.pagesValid.Size = new System.Drawing.Size(36, 36);
+            this.pagesValid.TabIndex = 8;
+            this.pagesValid.TabStop = false;
+            // 
+            // RunSave
+            // 
+            this.RunSave.Font = new System.Drawing.Font("Roboto", 13F, System.Drawing.FontStyle.Bold);
+            this.RunSave.Location = new System.Drawing.Point(542, 178);
+            this.RunSave.Name = "RunSave";
+            this.RunSave.Size = new System.Drawing.Size(150, 35);
+            this.RunSave.TabIndex = 9;
+            this.RunSave.Text = "Run, Save, Exit";
+            this.RunSave.UseVisualStyleBackColor = true;
+            this.RunSave.Click += new System.EventHandler(this.RunSave_Click);
+            // 
+            // PDFTK_CUT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 225);
+            this.Controls.Add(this.RunSave);
+            this.Controls.Add(this.pagesValid);
+            this.Controls.Add(this.pageTXTBox);
+            this.Controls.Add(this.pageSelector);
+            this.Controls.Add(this.SavePDFFile);
+            this.Controls.Add(this.outFileLocation);
+            this.Controls.Add(this.outputPDF);
+            this.Controls.Add(this.fileLocation);
+            this.Controls.Add(this.OpenPDFFile);
+            this.Controls.Add(this.inputPDF);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PDFTK_CUT";
+            this.Text = "PDFTK Cut";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pagesValid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputPDF;
+        private System.Windows.Forms.Button OpenPDFFile;
+        private System.Windows.Forms.OpenFileDialog openPDFDialog;
+        private System.Windows.Forms.Label fileLocation;
+        private System.Windows.Forms.SaveFileDialog savePDFDialog;
+        private System.Windows.Forms.Label outputPDF;
+        private System.Windows.Forms.Label outFileLocation;
+        private System.Windows.Forms.Button SavePDFFile;
+        private System.Windows.Forms.Label pageSelector;
+        private System.Windows.Forms.TextBox pageTXTBox;
+        private System.Windows.Forms.PictureBox pagesValid;
+        private System.Windows.Forms.Button RunSave;
+    }
+}
+
---
+++ WindowsFormsApp1/PDFTK_Cut.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class PDFTK_CUT : Form
+    {
+        private bool pagesREGEXValid = true;
+
+        public PDFTK_CUT()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OpenPDFFile_Click(object sender, EventArgs e)
+        {
+            openPDFDialog.ShowDialog();
+        }
+
+        private void openPDFDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            fileLocation.Text = openPDFDialog.FileName;
+        }
+
+        private void SavePDFFile_Click(object sender, EventArgs e)
+        {
+            savePDFDialog.ShowDialog();
+        }
+
+        private void savePDFDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            outFileLocation.Text = savePDFDialog.FileName;
+        }
+
+        private void pageTXTBox_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.Match(pageTXTBox.Text, "[1-9]+-[1-9]+").Success)
+            {
+                pagesREGEXValid = true;
+                pagesValid.Image = System.Drawing.Image.FromFile(".\\res\\checkmark.png");
+            }
+            else
+            {
+                pagesREGEXValid = false;
+                pagesValid.Image = System.Drawing.Image.FromFile(".\\res\\x.png");
+            }
+        }
+
+        private void RunSave_Click(object sender, EventArgs e)
+        {
+            if (pagesREGEXValid)
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = $"/C pdftk {fileLocation.Text} cat {pageTXTBox.Text} output {outFileLocation.Text}";
+                process.StartInfo = startInfo;
+                process.Start();
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Page Selection Is INVALID", "Page Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
